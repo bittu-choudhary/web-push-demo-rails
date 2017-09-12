31284f63121b1251bab854f32630fa33131ce78a
bittu-choudhary <bittu@moldedbits.com>
2017-09-12 13:34:01 +0530

    add webpush notifications controller

---
+++ app/controllers/web_push_notifications_controller.rb
@@ -0,0 +1,2 @@
+class WebPushNotificationsController < ApplicationController
+end
---
+++ test/controllers/web_push_notifications_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class WebPushNotificationsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
