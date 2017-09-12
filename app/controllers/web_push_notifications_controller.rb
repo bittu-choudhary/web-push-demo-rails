7730422276b95095989d720094c7613b3c835e84
bittu-choudhary <bittu@moldedbits.com>
2017-09-12 13:40:06 +0530

    add post push action in web push notifications controller

--- app/controllers/web_push_notifications_controller.rb
+++ app/controllers/web_push_notifications_controller.rb
@@ -2,4 +2,20 @@ class WebPushNotificationsController < ApplicationController
 
   def index
   end
+
+  def push
+    Webpush.payload_send(
+        ttl: 600,
+        message: params[:message],
+        endpoint: params[:subscription][:endpoint],
+        p256dh: params[:subscription][:keys][:p256dh],
+        auth: params[:subscription][:keys][:auth],
+        vapid: {
+            subject: "mailto:sender@example.com",
+            public_key: ENV['VAPID_PUBLIC_KEY'],
+            private_key: ENV['VAPID_PRIVATE_KEY']
+        }
+    )
+  end
+
 end
