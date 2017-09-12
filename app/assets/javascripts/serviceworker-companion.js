2943f892ca81e58089b06329720b6c15e61a613e
bittu-choudhary <bittu@moldedbits.com>
2017-09-12 13:38:23 +0530

    trigger web push on button click

--- app/assets/javascripts/serviceworker-companion.js
+++ app/assets/javascripts/serviceworker-companion.js
@@ -1,6 +1,26 @@
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register('/serviceworker.js', { scope: './' })
-    .then(function(reg) {
-      console.log('[Companion]', 'Service worker registered!');
-    });
+    navigator.serviceWorker.register('/serviceworker.js', { scope: './' })
+        .then(function(reg) {
+            console.log('[Companion]', 'Service worker registered!');
+        });
 }
+
+navigator.serviceWorker.ready.then(function(serviceWorkerRegistration) {
+  serviceWorkerRegistration.pushManager
+  .subscribe({
+    userVisibleOnly: true,
+    applicationServerKey: window.vapidPublicKey
+  });
+});
+
+$(function(){
+  $(".webpush-button").on("click", function(e) {
+    navigator.serviceWorker.ready
+    .then((serviceWorkerRegistration) => {
+      serviceWorkerRegistration.pushManager.getSubscription()
+      .then((subscription) => {
+        $.post("/push", { subscription: subscription.toJSON(), message: "You clicked a button!" });
+      });
+    });
+  });
+});
